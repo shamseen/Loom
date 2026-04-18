9194e4a8f34cbde7808b493f25a36424ba9bf991
shamseen <shamseen@bu.edu>
2026-04-18 10:58:25 -0500

    initial entry-service

---
+++ entry-service/src/main/java/com/example/entry_service/EntryServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.entry_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EntryServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EntryServiceApplication.class, args);
+	}
+
+}
---
+++ entry-service/src/test/java/com/example/entry_service/EntryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.entry_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EntryServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
