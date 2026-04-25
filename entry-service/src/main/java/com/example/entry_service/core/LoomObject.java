5bb2fb0085a1d90deb19f438a6a21d1486612971
shamseen <shamseen@bu.edu>
2026-04-24 20:31:04 -0500

    lombok-ify

--- entry-service/src/main/java/com/example/entry_service/core/LoomObject.java
+++ entry-service/src/main/java/com/example/entry_service/core/LoomObject.java
@@ -2,33 +2,31 @@ package com.example.entry_service.core;
 
 import java.time.ZoneOffset;
 import java.time.ZonedDateTime;
-import java.util.UUID;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.experimental.SuperBuilder;
 
-/*
+/**
+ * Subclasses should implement a static create() factory method which generates the id
+ * <p>Example: public static LoomEntry create(String title, ...) { ... }
  */
+@SuperBuilder            // Lombok: enables builder pattern for class hierarchies
+@Getter                  // Lombok: generates for all non-static fields
+@Setter
 public abstract class LoomObject {
+  // TODO: generate id from UUID
+  // this.id = getIdPrefix() + UUID.randomUUID().toString();
   private String id;
-  private Visibility visibility;
-  private ZonedDateTime createdAt;
+  @Builder.Default        // Lombok: sets default field value
+  private Visibility visibility = Visibility.PUBLIC;
+  @Builder.Default
+  private ZonedDateTime createdAt =  ZonedDateTime.now(ZoneOffset.UTC);
   private ZonedDateTime updatedAt;
 
+
   /**
-   * @return prefix string used to generate typed IDs
+   * @return prefix string used to generate typed IDs, must include separator
    */
   protected abstract String getIdPrefix();
-
-  public LoomObject(){
-    this(Visibility.PUBLIC);
-  }
-
-  /**
-   * 
-   * @param vis
-   */
-  public LoomObject(Visibility vis){
-    this.id = getIdPrefix() + "_" + UUID.randomUUID().toString();
-    this.createdAt = ZonedDateTime.now(ZoneOffset.UTC);
-    this.updatedAt = this.createdAt;
-    this.visibility = vis;
-  }
 }
